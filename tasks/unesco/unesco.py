094816500c28ce82ac8280d06b8a1201cd14c70e
Daniele <daoivot@tin.it>
2020-04-22 14:21:34 +0200

    UNESCO: EDU_NON_FINANCE dataflow read from csv

--- tasks/unesco/unesco.py
+++ tasks/unesco/unesco.py
@@ -19,270 +19,47 @@ UIS_PARAMS = {"url": "https://api.uis.unesco.org/sdmx/data/",
                   "Accept-Encoding": "gzip",
               }}
 
-sdg4_datalist = fileUtils.CsvUtils.readDictionaryFromCSV("Z:\\TransMonee\\src\\EDU_UIS_SDG4_toDown.csv")
+query_params = {"startPeriod": "2000", "endPeriod": "2050", "locale": "en"}
+
+'''
+     "filterOut": {
+         "UNIT_MEASURE": "GPIA"
+     }
+     '''
+
+datalist = fileUtils.CsvUtils.readDictionaryFromCSV("Z:\\TransMonee\\src\\EDU_UIS_SDG4_toDown.csv")
 _toProcess_UIS_dfSDG4 = []
-params = {"startPeriod": "2000", "endPeriod": "2050", "locale": "en"}
-for l in sdg4_datalist:
+for l in datalist:
     _toProcess_UIS_dfSDG4.append(
         {"dataflowId": [l['agency'], l['df'], l['ver']],
          "dq": l["dq"],
-         "params": params,
+         "params": query_params,
          "tmp_file": l["tmp_file"],
          "const": {
              "UNICEF_INDICATOR": l["UNICEF_INDICATOR"],
              "DATA_SOURCE": l["DATA_SOURCE"],
              "OBS_FOOTNOTE": l["OBS_FOOTNOTE"]
          }
-
          }
     )
 
-_toProcess_UIS_dfEDU_NON_FINANCE = [
-    {"dataflowId": ["UNESCO", "EDU_NON_FINANCE", "3.0"],
-     "dq": "STU.PER.L02.._T+F+M...INST_T..............AL+AM+AZ+BA+BG+BY+CZ+EE+GE+HR+HU+KG+KZ+LT+LV+MD+ME+MK+PL+RO+RS+RU+SI+SV+TJ+TM+TR+UA+UZ",
-     "params": {"startPeriod": "2000", "endPeriod": "2050", "locale": "en"},
-     "tmp_file": "EDU_01.csv",
-     "const": {
-         # "INDICATOR": "Pre-primary (ISCED 02) education enrolments (absolute number)",
-         "UNICEF_INDICATOR": "EDU_STU_L02_TOT",
-         "Dataflow": "ECARO:TRANSMONEE(1.0)",
-         "DATA_SOURCE": "UIS",
-         "OBS_FOOTNOTE": "",
-     }
-     },
-    {"dataflowId": ["UNESCO", "EDU_NON_FINANCE", "3.0"],
-     "dq": "STU.PER.L02.._T...INST_PUB..............AL+AM+AZ+BA+BG+BY+CZ+EE+GE+HR+HU+KG+KZ+LT+LV+MD+ME+MK+PL+RO+RS+RU+SI+SV+TJ+TM+TR+UA+UZ",
-     "params": {"startPeriod": "2000", "endPeriod": "2050", "locale": "en"},
-     "tmp_file": "EDU_02.csv",
-     "const": {
-         # "INDICATOR": "Pre-primary (ISCED 02) education enrolments in public institutions",
-         "UNICEF_INDICATOR": "EDU_STU_L02_PUB",
-         "Dataflow": "ECARO:TRANSMONEE(1.0)",
-         "DATA_SOURCE": "UIS",
-         "OBS_FOOTNOTE": "",
-     }
-     },
-    {"dataflowId": ["UNESCO", "EDU_NON_FINANCE", "3.0"],
-     "dq": "STU.PER.L02.._T...INST_PRIV..............AL+AM+AZ+BA+BG+BY+CZ+EE+GE+HR+HU+KG+KZ+LT+LV+MD+ME+MK+PL+RO+RS+RU+SI+SV+TJ+TM+TR+UA+UZ",
-     "params": {"startPeriod": "2000", "endPeriod": "2050", "locale": "en"},
-     "tmp_file": "EDU_03.csv",
-     "const": {
-         # "INDICATOR": "Pre-primary (ISCED 02) education enrolments in private institutions",
-         "UNICEF_INDICATOR": "EDU_STU_L02_PRIV",
-         "Dataflow": "ECARO:TRANSMONEE(1.0)",
-         "DATA_SOURCE": "UIS",
-         "OBS_FOOTNOTE": "",
-     }
-     },
-    {"dataflowId": ["UNESCO", "EDU_NON_FINANCE", "3.0"],
-     "dq": "GECER.PT.L02.._T.................AL+AM+AZ+BA+BG+BY+CZ+EE+GE+HR+HU+KG+KZ+LT+LV+MD+ME+MK+PL+RO+RS+RU+SI+SV+TJ+TM+TR+UA+UZ",
-     "params": {"startPeriod": "2000", "endPeriod": "2050", "locale": "en"},
-     "tmp_file": "EDU_04.csv",
-     "const": {
-         # "INDICATOR": "Enrolment in pre-primary (ISCED 02) education (gross enrolment ratio, % of population aged 3-6)",
-         "UNICEF_INDICATOR": "EDU_GECER_L02",
-         "Dataflow": "ECARO:TRANSMONEE(1.0)",
-         "DATA_SOURCE": "UIS",
-         "OBS_FOOTNOTE": "",
-     }
-     },
-    {"dataflowId": ["UNESCO", "EDU_NON_FINANCE", "3.0"],
-     "dq": "NER.PT.L02.._T.................AL+AM+AZ+BA+BG+BY+CZ+EE+GE+HR+HU+KG+KZ+LT+LV+MD+ME+MK+PL+RO+RS+RU+SI+SV+TJ+TM+TR+UA+UZ",
-     "params": {"startPeriod": "2000", "endPeriod": "2050", "locale": "en"},
-     "tmp_file": "EDU_05.csv",
-     "const": {
-         # "INDICATOR": "Enrolment in pre-primary (ISCED 02) education (net enrolment ratio, % of population aged 3-6)",
-         "UNICEF_INDICATOR": "EDU_NER_L02",
-         "Dataflow": "ECARO:TRANSMONEE(1.0)",
-         "DATA_SOURCE": "UIS",
-         "OBS_FOOTNOTE": "",
-     }
-     },
-    {"dataflowId": ["UNESCO", "EDU_NON_FINANCE", "3.0"],
-     "dq": "PRP..L02...................AL+AM+AZ+BA+BG+BY+CZ+EE+GE+HR+HU+KG+KZ+LT+LV+MD+ME+MK+PL+RO+RS+RU+SI+SV+TJ+TM+TR+UA+UZ",
-     "params": {"startPeriod": "2000", "endPeriod": "2050", "locale": "en"},
-     "tmp_file": "EDU_06.csv",
-     "const": {
-         # "INDICATOR": "Enrolment in private institutions of pre-primary (ISCED 02) education ( % of all children enrolled in respective level of education)",
-         "UNICEF_INDICATOR": "EDU_PRP_L02",
-         "Dataflow": "ECARO:TRANSMONEE(1.0)",
-         "DATA_SOURCE": "UIS",
-         "OBS_FOOTNOTE": "",
-     }
-     },
-    {"dataflowId": ["UNESCO", "EDU_NON_FINANCE", "3.0"],
-     "dq": "FEP..L02...................AL+AM+AZ+BA+BG+BY+CZ+EE+GE+HR+HU+KG+KZ+LT+LV+MD+ME+MK+PL+RO+RS+RU+SI+SV+TJ+TM+TR+UA+UZ",
-     "params": {"startPeriod": "2000", "endPeriod": "2050", "locale": "en"},
-     "tmp_file": "EDU_07.csv",
-     "const": {
-         # "INDICATOR": "Percentage of females in pre-primary (ISCED 02) education (% of all students enrolled in the respective level of education)",
-         "UNICEF_INDICATOR": "EDU_FEP_L02",
-         "Dataflow": "ECARO:TRANSMONEE(1.0)",
-         "DATA_SOURCE": "UIS",
-         "OBS_FOOTNOTE": "",
-     }
-     },
-    {"dataflowId": ["UNESCO", "EDU_NON_FINANCE", "3.0"],
-     "dq": "NERA...._T+F+M.UNDER1_AGE................AL+AM+AZ+BA+BG+BY+CZ+EE+GE+HR+HU+KG+KZ+LT+LV+MD+ME+MK+PL+RO+RS+RU+SI+SV+TJ+TM+TR+UA+UZ",
-     "params": {"startPeriod": "2000", "endPeriod": "2050", "locale": "en"},
-     "tmp_file": "EDU_08.csv",
-     "const": {
-         # "INDICATOR": "Participation in organized learning (Adjusted net enrolment rate, one year before official primary entry age - Administrative data) (SDG 4.2.2.)",
-         "UNICEF_INDICATOR": "EDU_NERA_UNDER1",
-         "Dataflow": "ECARO:TRANSMONEE(1.0)",
-         "DATA_SOURCE": "UIS",
-         "OBS_FOOTNOTE": "",
-     },
-     "filterOut": {
-         "UNIT_MEASURE": "GPIA"
-     }
-     },
-    {"dataflowId": ["UNESCO", "EDU_NON_FINANCE", "3.0"],
-     "dq": "NIR.PT.L1.._T+F+M.TH_ENTRY_AGE................AL+AM+AZ+BA+BG+BY+CZ+EE+GE+HR+HU+KG+KZ+LT+LV+MD+ME+MK+PL+RO+RS+RU+SI+SV+TJ+TM+TR+UA+UZ",
-     "params": {"startPeriod": "2000", "endPeriod": "2050", "locale": "en"},
-     "tmp_file": "EDU_09.csv",
-     "const": {
-         # "INDICATOR": "Net Intake Rate to primary education of official entry age",
-         "UNICEF_INDICATOR": "EDU_NIR_L1",
-         "Dataflow": "ECARO:TRANSMONEE(1.0)",
-         "DATA_SOURCE": "UIS",
-         "OBS_FOOTNOTE": "",
-     }
-     },
-    {
-        # Will generate duplicates if we get the Disaggregations by grade and type (Adult, initial).
-        "dataflowId": ["UNESCO", "EDU_NON_FINANCE", "3.0"],
-        "dq": "STU.PER.L1.._T+F+M...INST_T..............AL+AM+AZ+BA+BG+BY+CZ+EE+GE+HR+HU+KG+KZ+LT+LV+MD+ME+MK+PL+RO+RS+RU+SI+SV+TJ+TM+TR+UA+UZ",
-        "params": {"startPeriod": "2000", "endPeriod": "2050", "locale": "en"},
-        "tmp_file": "EDU_10.csv",
-        "const": {
-            # "INDICATOR": "Primary education (ISCED 1) enrolments (absolute number)",
-            "UNICEF_INDICATOR": "EDU_STU_L1",
-            "Dataflow": "ECARO:TRANSMONEE(1.0)",
-            "DATA_SOURCE": "UIS",
-            "OBS_FOOTNOTE": "",
-        }
-    },
-    {"dataflowId": ["UNESCO", "EDU_NON_FINANCE", "3.0"],
-     "dq": "FEP..L1...................AL+AM+AZ+BA+BG+BY+CZ+EE+GE+HR+HU+KG+KZ+LT+LV+MD+ME+MK+PL+RO+RS+RU+SI+SV+TJ+TM+TR+UA+UZ",
-     "params": {"startPeriod": "2000", "endPeriod": "2050", "locale": "en"},
-     "tmp_file": "EDU_11.csv",
-     "const": {
-         # "INDICATOR": "Percentage of females in primary  (ISCED 1) education (% of all students enrolled in the respective level of education)",
-         "UNICEF_INDICATOR": "EDU_FEP_L1",
-         "Dataflow": "ECARO:TRANSMONEE(1.0)",
-         "DATA_SOURCE": "UIS",
-         "OBS_FOOTNOTE": "",
-     }
-     },
-    {
-        # Do we need all the disaggr? Grade and Orientation (GENERAL, VOCATIONAL)
-        "dataflowId": ["UNESCO", "EDU_NON_FINANCE", "3.0"],
-        "dq": "STU.PER.L2.._T+F+M...INST_T..............AL+AM+AZ+BA+BG+BY+CZ+EE+GE+HR+HU+KG+KZ+LT+LV+MD+ME+MK+PL+RO+RS+RU+SI+SV+TJ+TM+TR+UA+UZ",
-        "params": {"startPeriod": "2000", "endPeriod": "2050", "locale": "en"},
-        "tmp_file": "EDU_12.csv",
-        "const": {
-            # "INDICATOR": "Lower Secondary education (ISCED 2) enrolments (absolute number)",
-            "UNICEF_INDICATOR": "EDU_STU_L2",
-            "Dataflow": "ECARO:TRANSMONEE(1.0)",
-            "DATA_SOURCE": "UIS",
-            "OBS_FOOTNOTE": "",
-        }
-    },
-    {
-        "dataflowId": ["UNESCO", "EDU_NON_FINANCE", "3.0"],
-        "dq": "FEP..L2...................AL+AM+AZ+BA+BG+BY+CZ+EE+GE+HR+HU+KG+KZ+LT+LV+MD+ME+MK+PL+RO+RS+RU+SI+SV+TJ+TM+TR+UA+UZ",
-        "params": {"startPeriod": "2000", "endPeriod": "2050", "locale": "en"},
-        "tmp_file": "EDU_13.csv",
-        "const": {
-            # "INDICATOR": "Percentage of females in lower secondary  (ISCED 2) education (% of all students enrolled in the respective level of education)",
-            "UNICEF_INDICATOR": "EDU_FEP_L2",
-            "Dataflow": "ECARO:TRANSMONEE(1.0)",
-            "DATA_SOURCE": "UIS",
-            "OBS_FOOTNOTE": "",
-        }
-    },
-    {
-        "dataflowId": ["UNESCO", "EDU_NON_FINANCE", "3.0"],
-        "dq": "STU.PER.L3._T._T+F+M...INST_T..............AL+AM+AZ+BA+BG+BY+CZ+EE+GE+HR+HU+KG+KZ+LT+LV+MD+ME+MK+PL+RO+RS+RU+SI+SV+TJ+TM+TR+UA+UZ",
-        "params": {"startPeriod": "2000", "endPeriod": "2050", "locale": "en"},
-        "tmp_file": "EDU_14.csv",
-        "const": {
-            # "INDICATOR": "Upper secondary education (ISCED 3) enrolment (absolute number)",
-            "UNICEF_INDICATOR": "EDU_STU_L3",
-            "Dataflow": "ECARO:TRANSMONEE(1.0)",
-            "DATA_SOURCE": "UIS",
-            "OBS_FOOTNOTE": "",
-        }
-    },
-    {
-        "dataflowId": ["UNESCO", "EDU_NON_FINANCE", "3.0"],
-        "dq": "FEP..L3...................AL+AM+AZ+BA+BG+BY+CZ+EE+GE+HR+HU+KG+KZ+LT+LV+MD+ME+MK+PL+RO+RS+RU+SI+SV+TJ+TM+TR+UA+UZ",
-        "params": {"startPeriod": "2000", "endPeriod": "2050", "locale": "en"},
-        "tmp_file": "EDU_15.csv",
-        "const": {
-            # "INDICATOR": "Percentage of females in upper secondary education (ISCED 3) (% of all students enrolled in the respective level of education)",
-            "UNICEF_INDICATOR": "EDU_FEP_L3",
-            "Dataflow": "ECARO:TRANSMONEE(1.0)",
-            "DATA_SOURCE": "UIS",
-            "OBS_FOOTNOTE": "",
-        }
-    },
-    {
-        "dataflowId": ["UNESCO", "EDU_NON_FINANCE", "3.0"],
-        "dq": "FEP..L3.C4..................AL+AM+AZ+BA+BG+BY+CZ+EE+GE+HR+HU+KG+KZ+LT+LV+MD+ME+MK+PL+RO+RS+RU+SI+SV+TJ+TM+TR+UA+UZ",
-        "params": {"startPeriod": "2000", "endPeriod": "2050", "locale": "en"},
-        "tmp_file": "EDU_16.csv",
-        "const": {
-            # "INDICATOR": "Percentage of females in general upper-secondary education  (% of all students enrolled in the respective level of education)",
-            "UNICEF_INDICATOR": "EDU_FEP_L3_C4",
-            "Dataflow": "ECARO:TRANSMONEE(1.0)",
-            "DATA_SOURCE": "UIS",
-            "OBS_FOOTNOTE": "",
-        }
-    },
-    {
-        "dataflowId": ["UNESCO", "EDU_NON_FINANCE", "3.0"],
-        "dq": "FEP..L3.C5..................AL+AM+AZ+BA+BG+BY+CZ+EE+GE+HR+HU+KG+KZ+LT+LV+MD+ME+MK+PL+RO+RS+RU+SI+SV+TJ+TM+TR+UA+UZ",
-        "params": {"startPeriod": "2000", "endPeriod": "2050", "locale": "en"},
-        "tmp_file": "EDU_17.csv",
-        "const": {
-            # "INDICATOR": "Percentage of females in vocational upper-secondary education  (% of all students enrolled in the respective level of education)",
-            "UNICEF_INDICATOR": "EDU_FEP_L3_C5",
-            "Dataflow": "ECARO:TRANSMONEE(1.0)",
-            "DATA_SOURCE": "UIS",
-            "OBS_FOOTNOTE": "",
-        }
-    },
-    {
-        "dataflowId": ["UNESCO", "EDU_NON_FINANCE", "3.0"],
-        "dq": "STU.PER.L3.C4._T+F+M...INST_T..............AL+AM+AZ+BA+BG+BY+CZ+EE+GE+HR+HU+KG+KZ+LT+LV+MD+ME+MK+PL+RO+RS+RU+SI+SV+TJ+TM+TR+UA+UZ",
-        "params": {"startPeriod": "2000", "endPeriod": "2050", "locale": "en"},
-        "tmp_file": "EDU_18.csv",
-        "const": {
-            # "INDICATOR": "Upper secondary enrolment by program orientation - General (number)",
-            "UNICEF_INDICATOR": "EDU_FEP_L3_C4",
-            "Dataflow": "ECARO:TRANSMONEE(1.0)",
-            "DATA_SOURCE": "UIS",
-            "OBS_FOOTNOTE": "",
-        }
-    },
-    {
-        "dataflowId": ["UNESCO", "EDU_NON_FINANCE", "3.0"],
-        "dq": "STU.PER.L3.C5._T+F+M...INST_T..............AL+AM+AZ+BA+BG+BY+CZ+EE+GE+HR+HU+KG+KZ+LT+LV+MD+ME+MK+PL+RO+RS+RU+SI+SV+TJ+TM+TR+UA+UZ",
-        "params": {"startPeriod": "2000", "endPeriod": "2050", "locale": "en"},
-        "tmp_file": "EDU_19.csv",
-        "const": {
-            # "INDICATOR": "Upper secondary enrolment by program orientation - Vocational (number)",
-            "UNICEF_INDICATOR": "EDU_FEP_L3_C5",
-            "Dataflow": "ECARO:TRANSMONEE(1.0)",
-            "DATA_SOURCE": "UIS",
-            "OBS_FOOTNOTE": "",
-        }
-    },
-    # UIS https://api.uis.unesco.org/sdmx/data/UNESCO,EDU_NON_FINANCE,3.0/STU.PER.L3.C5._T+F+M...INST_T..............AL+AM+AZ+BA+BG+BY+GE+HR+KG+KZ+MD+ME+MK+RO+RS+TJ+TM+TR+UA+UZ?startPeriod=2000&endPeriod=2050&format=sdmx-compact-2.0&locale=en&subscription-key=9d48382df9ad408ca538352a4186791b
+_toProcess_UIS_dfEDU_NON_FINANCE = []
+datalist = fileUtils.CsvUtils.readDictionaryFromCSV("Z:\\TransMonee\\src\\EDU_UIS_EDU_NON_FIN_toDown.csv")
 
-]
+for l in datalist:
+    _toProcess_UIS_dfEDU_NON_FINANCE.append(
+        {"dataflowId": [l['agency'], l['df'], l['ver']],
+         "dq": l["dq"],
+         "params": query_params,
+         "tmp_file": l["tmp_file"],
+         "const": {
+             "UNICEF_INDICATOR": l["UNICEF_INDICATOR"],
+             "DATA_SOURCE": l["DATA_SOURCE"],
+             "OBS_FOOTNOTE": l["OBS_FOOTNOTE"]
+         }
+         }
+    )
+
 
 colMap_UNESCO = {
     "Dataflow": {"type": "const", "value": "ECARO:TRANSMONEE(1.0)"},
@@ -352,14 +129,14 @@ def download_data(outfile_path, skipIfExists=False, verb=0):
         if verb >= 3:
             print(toDown['tmp_file'] + " " + res)
 
-    # for toDown in _toProcess_UIS_dfEDU_NON_FINANCE:
-    #     outFilePath = os.path.join(outfile_path, toDown['tmp_file'])
-    #     res = fileUtils.FileDownload.download_sdmx_file(outFilePath, UIS_PARAMS['url'], toDown['dataflowId'],
-    #                                                     toDown["dq"],
-    #                                                     params=toDown["params"], headers=UIS_PARAMS['headers'],
-    #                                                     apikey=UIS_API_KEY, skipIfExists=skipIfExists)
-    #     if verb >= 3:
-    #         print(toDown['tmp_file'] + " " + res)
+    for toDown in _toProcess_UIS_dfEDU_NON_FINANCE:
+        outFilePath = os.path.join(outfile_path, toDown['tmp_file'])
+        res = fileUtils.FileDownload.download_sdmx_file(outFilePath, UIS_PARAMS['url'], toDown['dataflowId'],
+                                                        toDown["dq"],
+                                                        params=toDown["params"], headers=UIS_PARAMS['headers'],
+                                                        apikey=UIS_API_KEY, skipIfExists=skipIfExists)
+        if verb >= 3:
+            print(toDown['tmp_file'] + " " + res)
 
 
 def _process(input_file, process, code_map, col_map, constants, check_for_dups=True):
@@ -387,7 +164,7 @@ def getdata(workingPath, cols):
     for p in _toProcess_UIS_dfSDG4:
         toAdd = _process(os.path.join(workingPath, p['tmp_file']), p, codemap_UNESCO, colMap_UNESCO, p['const'])
         ret = ret.append(toAdd)
-    # for p in _toProcess_UIS_dfEDU_NON_FINANCE:
-    #     toAdd = _process(os.path.join(workingPath, p['tmp_file']), p, codemap_UNESCO, colMap_UNESCO, p['const'])
-    #     ret = ret.append(toAdd)
+    for p in _toProcess_UIS_dfEDU_NON_FINANCE:
+        toAdd = _process(os.path.join(workingPath, p['tmp_file']), p, codemap_UNESCO, colMap_UNESCO, p['const'])
+        ret = ret.append(toAdd)
     return ret
