0b6f77069a4ad91f6b84e76db4856b605a7475e4
Daniele <daoivot@tin.it>
2020-04-28 18:25:28 +0200

    Transmonee excels code cleanup

--- tasks/transmonee_files/Transmonee_data.py
+++ tasks/transmonee_files/Transmonee_data.py
@@ -5,37 +5,50 @@ import pandas as pd
 
 class TransmoneeData:
 
-    def __init__(self, source_file):
-        self._source_file = source_file
+    # def __init__(self, source_file, destination_cols):
+    #     self._source_file = source_file
+    #     self._destination_cols = destination_cols
 
-    def _process(self, data, col_map, constants, codeMap=None):
-        # print(data.head())
-        colMapper = ColumnMapper.ColumnMapper(col_map)
-
-        if codeMap is not None:
-            for col in codeMap:
-
-                if "depends" in codeMap[col]:
-                    for m in codeMap[col]['map']:
-                        sourceCol = codeMap[col]["depends"]
-
-                        mask = data[sourceCol] == m
-
-
-                        data[col][mask] = codeMap[col]["map"][m]
+    # def _process(self, data, col_map, constants, codeMap=None):
+    #     # print(data.head())
+    #     colMapper = ColumnMapper.ColumnMapper(col_map)
+    #
+    #     mapped = colMapper.mapDataframe(data, constants)
+    #     return mapped
 
 
 
-                else:
-                    for m in codeMap[col]:
-                        data[col].replace(m, codeMap[col][m], inplace=True)
+    def readSource(self, source_file):
+        data=parser.parse_transmonee_file(source_file)
+        return data
 
-        mapped = colMapper.mapDataframe(data, constants)
-        return mapped
 
-    def getdata(self, cols, columnMap, constVals, filterFunction=None, codeMap=None):
-        ret = pd.DataFrame(columns=cols, dtype=str)
-        srcdata = parser.parse_transmonee_file(self._source_file)
-        srcdata = self._process(srcdata, columnMap, constVals, codeMap)
-        ret = ret.append(srcdata)
-        return ret
+
+    def map_codes(self, data, codemap):
+        DEP = "depends"
+        MAP = "map"
+        if codemap is None:
+            return data
+
+        for col in codemap:
+            if DEP in codemap[col]:
+                for m in codemap[col][MAP]:
+                    sourceCol = codemap[col][DEP]
+                    mask = data[sourceCol] == m
+                    data[col][mask] = codemap[col][MAP][m]
+            else:
+                for m in codemap[col]:
+                    data[col].replace(m, codemap[col][m], inplace=True)
+
+        return data
+
+    def mapcols(self, data, colmap, consts):
+        colmapper = ColumnMapper.ColumnMapper(colmap)
+        return colmapper.mapDataframe(data, consts)
+
+    # def getdata(self, cols, columnMap, constVals, filterFunction=None, codeMap=None):
+    #     ret = pd.DataFrame(columns=cols, dtype=str)
+    #     srcdata = parser.parse_transmonee_file(self._source_file)
+    #     srcdata = self._process(srcdata, columnMap, constVals, codeMap)
+    #     ret = ret.append(srcdata)
+    #     return ret
